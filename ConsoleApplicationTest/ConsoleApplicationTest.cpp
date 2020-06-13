ecdd06ed4228f95ae269d8f418a7c49e976624c8
robin roussac <Robinroussac@gmail.com>
2020-06-13 17:43:46 +0200

    3eme jours

--- ConsoleApplicationTest/ConsoleApplicationTest.cpp
+++ ConsoleApplicationTest/ConsoleApplicationTest.cpp
@@ -3,20 +3,21 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main()
-{
-    cout << "nique ta mere shadow" << endl << "car t'es un gros fdp" << endl;
+{   
+   /* cout << "t'es chiant florent" << endl << "car t'es un debutant" << endl;
     cout << "je teste des choses" << " car je suis debutant" << endl; 
 
 
-    string phrase("bienvenue dans question pour un noob !");
+    string phrase("bienvenue dans question pour un coince !");
     bool estCeUneBonneIdee(true), vousEtesSur(true);
-    int depuisCombienDeTempsShadowEstCon(16);
+    int depuisCombienDeTempsShadowEstPasTresIntelligent(16);
     // double marche pas?
 
-    cout << phrase << endl << "depuis combien de temps shadow est con ?" << endl << depuisCombienDeTempsShadowEstCon <<endl;
+    cout << phrase << endl << "depuis combien de temps shadow est con ?" << endl << depuisCombienDeTempsShadowEstPasTresIntelligent <<endl;
 
     int ageUtilisateur(18); //Une variable pour contenir l'âge de l'utilisateur
 
@@ -32,7 +33,48 @@ int main()
     cout << "Vous avez " << maReference << " ans. (via reference)" << endl;
     //Et on affiche en utilisant la référence
 
-    //suite du cours diamnche...
+     int quelleage(0);
+        cout << "quelle age avez vous ?" << endl;
+        cin >> quelleage; 
+        cout << "vous avez : " << quelleage << " ans" << endl; */
+        
+        
+        cout << "Quel est votre nom ?" << endl;
+        string nomUtilisateur("Sans nom"); //On crée une case mémoire pour contenir une chaine de caractères
+        getline(cin, nomUtilisateur); //On remplit cette case avec toutela ligne que l'utilisateur a écrit
+
+        cout << "Combien vaut pi ?" << endl;
+        double piUtilisateur(-1.); //On crée une case mémoire pour stockerun nombre réel
+        cin >> piUtilisateur; //Et on remplit cette case avec ce qu'écritl'utilisateur
+     
+        cout << "Vous vous appelez " << nomUtilisateur << " et vous pensez que pi vaut " << piUtilisateur << "." << endl;
+
+        int const nombreNiveaux(10);
+        
+        int a(0), b(0), resultat(0);
+        cout << "Choississez unze valeure sans virgule pour a : " << endl;
+        cin >> a;
+        cout << "choississzez une valeure sans virgule pour b : " << endl;
+        cin >> b;
+        resultat = a + b;
+        ++resultat;
+        --resultat;
+        cout << "le resultat est " << resultat << endl;
+
+        double nombre(5.3);
+        nombre += 4.2;       //'nombre' vaut maintenant 9.5
+        nombre *= 2.;        //'nombre' vaut maintenant 19
+        nombre -= 1.;        //'nombre' vaut maintenant 18
+        nombre /= 3.;        //'nombre' vaut maintenant 6
+        cout << "le resultat est " << nombre << endl; 
+
+        double const a(19);
+        double const b(5);
+        double const resultat = pow(a, b); // creer un nombre avec des puissances
+
+
+    //suite du cours dimanche...
+
 
 
     return 0; //termine la fonction main et termine le programme
